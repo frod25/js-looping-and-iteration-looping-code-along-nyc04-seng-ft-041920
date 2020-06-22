3f2b44236685c5eac7a0e9c5fed2320d35fcdde5
frod25 <frod25>
2020-06-22 18:03:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(arr, eventName) {
+    let newArr = []
+    for (let i = 0; i < arr.length; i++) {
+        newArr.push(`Thank you, ${arr[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return newArr
+}
+
+function countDown(nbr) {
+    while (nbr >= 0) {
+        console.log(nbr)
+        nbr--
+    }
+}
